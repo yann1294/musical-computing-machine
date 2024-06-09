bd73730e59fd9994c139d6110d18a992dd5345ad
Yann <dev.yann12@gmail.com>
2024-06-09 18:04:57 +0530

    removed event handler on navigation component

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <>
         <TopBar/>
-        <Navigation toggle={()=>{}}/>
+        <Navigation/>
         <br />
         {children}
         </>
